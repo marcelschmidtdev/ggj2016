712c0ddd68cba45a4435e6cf5afe6fc642a900af
Marcel Schmidt <marcelschmidt1337@gmail.com>
2016-01-29 22:28:25 +0100

    Added dummy Minion prefab, started CreepSpawner script, added LevelEditor tag

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour
+{
+	[SerializeField]
+	private float spawnRateInSeconds;
+	[SerializeField]
+	private int amountOfCreeps;
+	[SerializeField]
+	private GameObject[] AvailableCreeps;
+
+	public int PlayerId { get; private set; }
+
+	//TODO register at game instance singleton and get game start message
+	private bool gameStarted = false;
+
+	void Update () {
+		if(gameStarted) {
+
+		}
+	}
+}
