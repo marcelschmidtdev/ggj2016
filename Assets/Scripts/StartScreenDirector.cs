6acd339068f03cbdbcfb4a8f59572eb44db3fcbc
Friedrich Wessel <mail@friedrichwessel.de>
2016-01-31 03:01:45 +0100

    startScreen

---
+++ Assets/Scripts/StartScreenDirector.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class StartScreenDirector : MonoBehaviour {
+
+
+	public List<Animator> Actors; 
+	public AudioSource BackgroundSound; 
+	public GameObject MeteorStorm; 
+	public Animator Player; 
+	public float TimeTillMeteor; 
+	public float TimeTillDisable;
+	public float TimeTillPlayer; 
+	public float TimeTillEnd; 
+	public float TimeTillMinionDie; 
+	public float AudioOffset; 
+	public float AudioFadeoff; 
+	public float AudioFadeIn; 
+
+	void Start () {
+		foreach( var actor in this.Actors ) {
+			actor.SetTrigger("Rock"); 
+		}
+		this.BackgroundSound.time = this.AudioOffset; 
+		this.BackgroundSound.volume = 0; 
+		this.BackgroundSound.Play(); 
+
+	}
+
+	void Update(){
+		if ( this.BackgroundSound.volume < 1 ) {
+			this.BackgroundSound.volume += Time.deltaTime * this.AudioFadeIn; 
+		}
+		this.TimeTillMeteor -= Time.deltaTime; 
+		if ( this.TimeTillMeteor <= 0 ) {
+			StartMeteor(); 
+			this.TimeTillPlayer -= Time.deltaTime; 
+			if ( this.TimeTillPlayer <= 0 ) {
+				StartPlayer(); 
+				this.TimeTillMinionDie -= Time.deltaTime; 
+				if ( this.TimeTillMinionDie <= 0) {
+					foreach( var actor in this.Actors ) {
+						actor.SetTrigger("Kill"); 
+					}
+				}
+				this.TimeTillEnd -= Time.deltaTime; 
+				if (this.TimeTillEnd <= 0){
+					Application.LoadLevel("MainMenu"); 
+				}
+				this.TimeTillDisable -= Time.deltaTime; 
+				if ( this.TimeTillDisable <= 0 ) {
+					this.MeteorStorm.SetActive( false ) ; 
+					this.BackgroundSound.volume -= Time.deltaTime * this.AudioFadeoff; 
+				}
+			} 
+
+		} 
+
+	}
+
+	void StartPlayer(){
+		this.Player.SetTrigger("Kill"); 
+	}
+
+	void StartMeteor(){
+		this.MeteorStorm.SetActive(true); 
+	}
+	
+
+}
