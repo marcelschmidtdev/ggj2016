5bc3f190518b45356b1d9ebffb748520728dc3c9
Malcolm <crummynz@gmail.com>
2016-01-31 01:34:07 +0100

    Begun work on exploder

---
+++ Assets/Exploder.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exploder : MonoBehaviour {
+    private ArrayList playersInRange;
+    private ArrayList minionsInRange;
+
+	// Use this for initialization
+	void Start () {
+        playersInRange = new ArrayList();
+        minionsInRange = new ArrayList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "player")
+        {
+            playersInRange.Add(other.gameObject);
+        } else if (other.name == "minion")
+        {
+            minionsInRange.Add(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "player")
+        {
+            playersInRange.Remove(other.gameObject);
+        }
+        else if (other.name == "minion")
+        {
+            minionsInRange.Remove(other.gameObject);
+        }
+    }
+}
